bccd0bce470dbf30c96772c8c492a03b60cd1a97
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-01-14 19:57:54 +0900

    Update 1018.cpp

--- codeup/1018.cpp
+++ codeup/1018.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     int a,b;
-    char x;
-    cin >> a >> x >> b;
+    char x; 
+    cin >> a >> x >> b; //중간에 ":"를 문자로 따로 b에 저장
     cout << a << x << b << endl;
     
     return 0;
