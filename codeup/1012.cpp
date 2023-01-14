810a3037de53bdf2475a22341a76be15a9dacae2
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-01-14 19:55:14 +0900

    Create 1012.cpp

---
+++ codeup/1012.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+    
+    double a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(6);
+    cout << a << endl;
+    
+    return 0;
+}
