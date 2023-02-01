0dc2bb1837cdf8c34008f12f8b8fd713003a48e2
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-02-01 18:39:32 +0900

    Create 1023.cpp

---
+++ codeup/1023.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,c;
+    char b;
+    
+    cin >> a >> b >> c;
+    
+    cout << a << endl;
+    cout << c << endl;
+    
+    return 0;
+}
