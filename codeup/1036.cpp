c7c4a4a3f2feb8995cb96358842144ce271ec6ec
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-02-05 20:41:28 +0900

    Create 1036.cpp

---
+++ codeup/1036.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    
+    scanf("%c", &c); // cin cout 못쓰는건 어쩔수 없나 보다. 나중에 방법 찾아보기로
+    printf("%d", c);
+    
+    return 0;
+}
