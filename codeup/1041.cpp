883587ad075f0e1958289847a6d999ee63a6bed7
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-02-05 20:54:20 +0900

    Create 1041.cpp

---
+++ codeup/1041.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    
+    cin >> c;
+    cout << (char)(c+1) << endl; // char에 정수 입력받아서 c+1 하고 char로 다시 출력
+    
+    return 0;
+}
