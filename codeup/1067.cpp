14031262e73825cdc236690a121370911805ef76
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-03-02 12:53:36 +0900

    Create 1067.cpp

---
+++ codeup/1067.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    
+    cin >> a;
+    
+    if (a>0)
+    {
+        printf("plus\n");
+    }
+    else
+    {
+        printf("minus\n");
+    }
+    if (a%2 ==0)
+    {
+        printf("even\n");
+    }
+    else
+    {
+        printf("odd\n");
+    }
+    
+    
+}
