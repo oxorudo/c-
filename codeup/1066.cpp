759f50e52ce785714506fb3c869c3967c881e929
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-03-02 11:49:27 +0900

    Create 1066.cpp

---
+++ codeup/1066.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    if (a%2 ==0)
+    {
+        printf("even\n");
+    }
+    else
+    {
+        printf("odd\n");
+    }
+    if (b%2 ==0)
+    {
+        printf("even\n");
+    }
+    else
+    {
+        printf("odd\n");
+    }
+    if (c%2 ==0)
+    {
+        printf("even\n");
+    }
+    else
+    {
+        printf("odd\n");
+    }
+}
