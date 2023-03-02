4ed5ce8acf8ac181691fc1f145120f0303d3d9d7
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-03-02 12:55:38 +0900

    Create 1068.cpp

---
+++ codeup/1068.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    
+    cin >> a;
+    
+    if (a>=90)
+    {
+        printf("A\n");
+    }
+    else if (a>=70)
+    {
+        printf("B\n");
+    }
+    else if (a>=40)
+    {
+        printf("C\n");
+    }
+    else
+    {
+        printf("D\n");
+    }
+    
+    
+}
