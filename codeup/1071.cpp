e94e1dbd700ecfa957c4fde36d3242463c2121e8
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-03-09 00:14:33 +0900

    Update 1071.cpp

--- codeup/1071.cpp
+++ codeup/1071.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
      int n;
-     reload: 
+     reload: //레이블 이름 : reload
      scanf("%d", &n);
-     if(n!=0){
+     if(n!=0){ // n이 0이 아닐때만 n 출력해주고 goto reload;
       printf("%d\n", n);
       goto reload;
       }
