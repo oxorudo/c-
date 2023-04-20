0b2d3010bc017ea08a094ddce8fc47144e915a20
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-04-20 17:34:07 +0900

    Update 1080.cpp

--- codeup/1080.cpp
+++ codeup/1080.cpp
@@ -6,13 +6,12 @@ int main()
     int a;
     int b=0;
     int sum=0;
-    int count=0;
     scanf("%d",&b);
     for(a=1; a<=100; a++){
         sum+=a;
-        count++;
         if(sum>=b) break;
         }
-        printf("%d",count);
+        printf("%d",a);
     
 }
+
