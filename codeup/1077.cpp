f1636879dd113544c20a1683db0280364695aea3
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-04-20 16:48:49 +0900

    Create 1077.cpp

---
+++ codeup/1077.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    scanf("%d",&b);
+    
+    for(a=0; a<=b; a++)
+    {
+        printf("%d\n",a);
+        
+    }
+    return 0;
+}
