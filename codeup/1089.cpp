b922555f13deddc55480f3f78effea65f96a0d54
oxorudo <87151398+oxorudo@users.noreply.github.com>
2023-05-16 11:17:25 +0900

    Create 1089.cpp

---
+++ codeup/1089.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,d,n,result;
+    
+    scanf("%d %d %d",&a,&d,&n);
+    
+    result = a+(n-1)*d;
+    
+    printf("%d", result);
+    
+}
